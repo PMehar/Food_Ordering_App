417a040697b884a419efe8c25d78e5c0c3f6fd5f
pujamehar05 <meharpuja02@gmail.com>
2024-08-25 13:54:05 +0530

    Filtered list of restaurants using useState() hook and onClick event handler

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,31 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
 
 const Body = ()=>{
+
+    const [listOfRestaurants , setListOfRestaurants] = useState(resList)
+
     return(
         <div className ="body">
-            <div className="search">Search</div>
+            {/* <div className="search">Search</div> */}
+
+            <div className="filter">
+                
+                <button className="filter-btn" onClick={()=>{
+                    // filter logic here..
+                    const filteredList = listOfRestaurants.filter((res)=>
+                        res.info.avgRating>4.1
+                    );
+                    setListOfRestaurants(filteredList);
+                    console.log(listOfRestaurants);
+                }}>Top Rated Restaurants</button>
+               
+            </div>
+
             <div className="res_Container">
                 {/* restaurant cards */}
-               {resList.map((restaurant)=>(
+               {listOfRestaurants.map((restaurant)=>(
                 <RestaurantCard key={restaurant?.info?.id} resData={restaurant}/>
                ))}  
                
