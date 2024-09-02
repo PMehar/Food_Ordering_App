13fd99cc2a7807753b7cf2b285b756f38660ba98
pujamehar05 <meharpuja02@gmail.com>
2024-09-02 12:35:58 +0530

    created & updated states inside class Based Components & Fuctional Component

--- src/components/User.js
+++ src/components/User.js
@@ -1,6 +1,19 @@
+import { useState } from "react";
+
 const User = ({name}) =>{
+    const [count , setCount] = useState(0);
+    const [count2 , setCount2] = useState(2);
+
     return(
         <div className="user-card">
+            <h2>Count = {count}</h2>
+            
+            <button onClick={()=>{
+                setCount(count+1);
+                }}>Count increase
+            </button>
+
+            <h2>Count2 = {count2}</h2>
             <h2>Name : {name}</h2>
             <h3>Location : Dehradun</h3>
             <h4>Contact : @puja05</h4>
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -3,12 +3,30 @@ import React from "react";
 class UserClass extends React.Component{
     constructor(props){
         super(props);
-        console.log(props);
+
+        this.state = {
+            count: 0,
+            count2: 2,
+        };
     }
+
     render(){
+        const{count, count2} = this.state;
+        const{name } = this.props;
         return(
             <div className="user-card">
-                <h2>Name : {this.props.name}</h2>
+                <h1>Count : {count}</h1>
+            
+                <button onClick={()=>{
+                    // Never Update State Variable directly
+                    this.setState({
+                        count: this.state.count+1,
+                    });
+                    }}>Count Increase
+                </button>
+
+                <h1>Count2 : {count2}</h1>
+                <h2>Name : {name}</h2>
                 <h3>Location : Dehradun</h3>
                 <h4>Contact : @puja05</h4>
             </div>
